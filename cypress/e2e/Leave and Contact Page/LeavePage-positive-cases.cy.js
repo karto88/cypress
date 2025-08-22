23b1f8c3f36d309d331f5c2fa137526beaa112c0
kaarto <the.karto.88@gmail.com>
2025-08-22 23:55:57 +0400

    add new leave pages cases

--- cypress/e2e/Leave and Contact Page/LeavePage-positive-cases.cy.js
+++ cypress/e2e/Leave and Contact Page/LeavePage-positive-cases.cy.js
@@ -1,20 +1,11 @@
+/// <reference types="cypress" />
 
 const {generateRandomEmail} = require ('../../support/generatemail');
 
 
 describe('RangeHRM login', () => {
 
-  it('log in with correctly', () => {
-
-    cy.loginapp('Admin','admin123')
-
-    const username = 'Bhoomika rn'
-    cy.get('.oxd-userdropdown-name').then((usernamecorrectly) => {
-      const actualname = usernamecorrectly.text();
-      expect(actualname).to.equal(username)
-    }) 
-  }) 
-
+  
   it('live list - search with date', () => {
     cy.loginapp('Admin','admin123')
     cy.get(':nth-child(3) > .oxd-main-menu-item').click()
@@ -59,8 +50,10 @@ describe('RangeHRM login', () => {
       cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div[2]/div/div[1]/button[2]')
       .click();
     }
-    cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div[2]/div/div[1]/button[2]')
-    .should('have.text','April')
+    cy.get('.oxd-calendar-selector-month-selected > .oxd-text', { timeout: 10000 })
+  .invoke('text').then(text => {
+    expect(text).to.eq('December')
+  });
   })
 
 it('live list - Clicks next month button 4 times right', () => {
@@ -79,7 +72,7 @@ it('live list - Clicks next month button 4 times right', () => {
     .should('have.text','December')
   })
 
-  it.only('select all dropdowns', () => {
+  it('select all dropdowns', () => {
 
      cy.loginapp('Admin','admin123')
 
@@ -118,45 +111,46 @@ it('live list - Clicks next month button 4 times right', () => {
     //let SubUnit = 'Marketing'
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click();
     cy.xpath('//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/div/div[2]/div/div[2]/div/div[2]/div[8]')
-    .should('have.text','Marketing').click();
+    .eq(0).click();
   
     
    cy.get('.oxd-button--secondary').click() // search
-    const notfoundtext = 'No Records Found'
-    cy.get('.orangehrm-header-container > .oxd-text').then((checknotfound) => {
+
+    const notfoundtext = 'InfoNo Records Found'
+    cy.get('.oxd-toast p').then((checknotfound) => {
       const exptext = checknotfound.text();
       expect(exptext).to.equal(notfoundtext)
     })
 
   })
-  it ('write user info - contact details', () => {
+  it('write user info - contact details', () => {
 
    cy.loginapp('Admin','admin123')
 
    cy.get(':nth-child(6) > .oxd-main-menu-item > .oxd-text').click()
    cy.get(':nth-child(2) > .orangehrm-tabs-item').click()
    cy.get(':nth-child(3) > .oxd-grid-3 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input')
-   .type('Street 1')
+   .clear().type('Street 1')
    cy.get(':nth-child(3) > .oxd-grid-3 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
-   .type('Street 2')
+   .clear().type('Street 2')
    cy.get(':nth-child(3) > .oxd-grid-3 > :nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
-   .type('tbilisi');
+   .clear().type('tbilisi');
    cy.get(':nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-input')
-   .type('tbilisi');
+   .clear().type('tbilisi');
    cy.get(':nth-child(5) > .oxd-input-group > :nth-child(2) > .oxd-input')
-   .type('32123')
+   .clear().type('32123')
    cy.get('.oxd-select-text').click()
    cy.contains('span', 'Georgia', { timeout: 10000 }).scrollIntoView().should('be.visible').click()
     cy.get(':nth-child(6) > .oxd-grid-3 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input')
-    .type('+995591000001')
+    .clear().type('+995591000001')
     cy.get(':nth-child(6) > .oxd-grid-3 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
     .clear().type('937277728');
     cy.get(':nth-child(6) > .oxd-grid-3 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
-    .type('234-123-11')
+    .clear().type('234-123-11')
     cy.get(':nth-child(9) > .oxd-grid-3 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input')
     .clear().type(generateRandomEmail());
     cy.get(':nth-child(9) > .oxd-grid-3 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
-    .type(generateRandomEmail());
+    .clear().type(generateRandomEmail());
     cy.wait(2000)
     cy.get('.oxd-form-actions > .oxd-button').click()
     
