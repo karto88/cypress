b87478c9271b35760a31628dd2566ac762867002
kaarto <the.karto.88@gmail.com>
2025-08-31 20:43:54 +0400

    add new test cases

--- cypress/e2e/admin/create-pay.grandes.cy.js
+++ cypress/e2e/admin/create-pay.grandes.cy.js
@@ -12,6 +12,7 @@ describe('pay grandes', () => {
         login.setusername('Admin')
         login.setpassword('admin123')
         login.loginbutton()
+        
     })
 
     it('create pay grandes', () => {
@@ -19,9 +20,38 @@ describe('pay grandes', () => {
         const name = Cypress.env('Grandesname')
 
         cy.createPayGrandes();
+
         cy.wait(3000)
         cy.get('.oxd-table-body', {timeout: 1000}, name).should('be.visible')
 
+    })
+
+    it('delete pay grandes', () => {
+
+        const name =Cypress.env('Grandesname')
+
+        cy.createPayGrandes();
+
+        cy.get('.oxd-table-body button').eq(0).click()
+        cy.get('.oxd-button--label-danger').click()
+        cy.contains('.oxd-table-body', name).should('not.exist')
+    })
+
+    it.only('edit name pay grandes', () => {
+        
+        cy.createPayGrandes();
+
+        cy.get('.oxd-table-body button').eq(1).click()
+        cy.get(':nth-child(2) > .oxd-input').clear().type('test grandes')
+        cy.get('.oxd-form-actions > .oxd-button--secondary').click()
+
+        cy.get(':nth-child(2) > .oxd-topbar-body-nav-tab-item').click()
+        cy.get('.oxd-dropdown-menu').find('li').eq(1).click()
+        cy.contains('.oxd-table-body', 'test grandes').should('not.exist')
+
+
+        
+        
     })
 
 
